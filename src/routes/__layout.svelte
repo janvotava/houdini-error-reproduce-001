16ccba51a54d2af080c28a11b7054b4be4b19976
Jan Votava <votava@deployment.cz>
2022-08-04 11:27:58 +0700

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script context="module" lang="ts">
+	import client from '../client'
+
+	client.init()
+</script>
+
+<script lang="ts">
+  import { GQL_Characters } from "$houdini"
+</script>
+
+<!-- Comment this line out and it will start working -->
+{$GQL_Characters}
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" context="module">
+  import type { Load } from "@sveltejs/kit"
+
+  export const load: Load = async (event) => {
+    await GQL_Characters.fetch({
+      event,
+    })
+
+    return {}
+  }
+</script>
+
+<script lang="ts">
+  import { GQL_Characters } from "$houdini"
+  import { browser } from "$app/env"
+
+  $: browser && GQL_Characters.fetch()
+</script>
+
+<pre>
+  {JSON.stringify($GQL_Characters.data, null, 2)}
+</pre>
